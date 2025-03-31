932a98e1b4a5a6ccc34937424331cc13961aac85
Omar Roman <omar.roman.2828@gmail.com>
2025-03-31 13:31:29 +0000

    detección de imagenes

---
+++ code/python/sin_tipado/analisis_patrones_inferencia.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+modelo = tf.keras.models.load_model("modelo_perceptronFigG.keras")
+
+imagen = cv2.imread("imagen")
+imagen = cv2.resize(imagen, (64, 64))
+cv2.imshow("Imagen", imagen)
+imagen = cv2.cvtColor(imagen.cv2.COLOR_BGR2GRAY)
+borde = cv2.Canny(imagen, 100, 200)
+imagen = borde.astype("float32") / 255.0  # Normalizar entre 0 y 1
+
+
+x_test = tf.convert_to_tensor(
+    np.array(imagen).reshape((1, 64, 64, 1)),dtype=tf.float32)
+)
+
+prediccion=modelo.predict(x_test)
+prediccion_int = np.argmax(prediccion)
+print(prediccion,"      ",prediccion_int)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
